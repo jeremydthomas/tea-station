f681b069f6a192c0e69b2d8004fb7b702cf68eb3
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-05-03 22:19:15 -0500

    navbar

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // setup nav
-
+const navBtn = document.getElementById('nav-btn');
+const navBar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
 // show nav
-
 
 // close nav
 
 //setup date
+
+const date = (document.getElementById('date').innerHTML =
+	new Date().getFullYear());
 
