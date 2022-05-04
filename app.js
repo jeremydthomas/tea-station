2b59b889736217b4eac376d69afdfbd45cf5776b
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-05-04 15:39:54 -0500

    added javascript to app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const navBtn = document.getElementById('nav-btn');
 const navBar = document.getElementById('navbar');
 const navClose = document.getElementById('nav-close');
 // show nav
+navBtn.addEventListener('click', () => {
+	navBar.classList.add('showNav');
+});
 
 // close nav
-
+navClose.addEventListener('click', () => {
+	navBar.classList.remove('showNav');
+});
 //setup date
 
 const date = (document.getElementById('date').innerHTML =
 	new Date().getFullYear());
-
